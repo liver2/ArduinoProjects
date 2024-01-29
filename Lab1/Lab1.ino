0737fb8c47e6788f61ca866b5a10baa13fab908c
liver2 <124078178+liver2@users.noreply.github.com>
2024-01-29 10:34:52 -0500

    Update Lab1.ino

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,14 +1,14 @@
 // Oliver Szabo
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT); // Setting the different pins to output mode so that they will recognize digitalWrite()
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13, HIGH); 
+  digitalWrite(13, HIGH); // Sends a 5V voltage to the pin that causes current and a potential difference, lighting up the LED
   delay(5000);                      
-  digitalWrite(13, LOW);
+  digitalWrite(13, LOW); // Sends a 0V voltage to the pin, causing the LED to go out
   digitalWrite(11, HIGH);  
   delay(5000);
   digitalWrite(11, LOW);
