80975ff18a1dc5e4e0537d57b2f3c86afb30cbd6
liver2 <124078178+liver2@users.noreply.github.com>
2024-01-31 10:14:40 -0500

    Update Circuit2.ino

--- Lab2/Circuit2.ino
+++ Lab2/Circuit2.ino
@@ -1,1 +1,12 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  // pinMode(A0, INPUT); <-- not working correctly
+}
 
+void loop() {
+  int potentiometerValue = analogRead(A0);
+  digitalWrite(13, HIGH);
+  delay(potentiometerValue);
+  digitalWrite(13, LOW);
+  delay(potentiometerValue);
+}
