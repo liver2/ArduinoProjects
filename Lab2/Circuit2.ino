73d8bf061e8843e725321532e0855f99f1a78bc0
liver2 <124078178+liver2@users.noreply.github.com>
2024-02-02 10:33:11 -0500

    Update Circuit2.ino

--- Lab2/Circuit2.ino
+++ Lab2/Circuit2.ino
@@ -1,12 +1,14 @@
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600); // Set up serial monitor
   // pinMode(A0, INPUT); <-- not working correctly
 }
 
 void loop() {
-  int potentiometerValue = analogRead(A0);
-  digitalWrite(13, HIGH);
-  delay(potentiometerValue);
+  int potentiometerValue = analogRead(A0); // The analog pin reads a value from the potentiometer's voltage output
+  Serial.print(potentiometerValue);
+  digitalWrite(13, HIGH); // LED lights up
+  delay(potentiometerValue); // Delays according to the degree of voltage
   digitalWrite(13, LOW);
   delay(potentiometerValue);
 }
